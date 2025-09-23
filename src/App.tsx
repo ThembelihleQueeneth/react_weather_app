4a77963064f73fe596996d4f4b5b9d6f5d506116
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2025-09-23 16:06:32 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return(
+  <>
+  <h1>Weather App</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rerum itaque fugit officiis mollitia repellat corporis optio dicta ut! Cumque, reprehenderit a? Esse sit accusamus magnam pariatur veniam suscipit alias.</p>
+  </>
+  )
+}
+
+export default App
